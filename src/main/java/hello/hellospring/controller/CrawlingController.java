6fae708f0cbbed2820c03eb3d9e1aeb1baf6d0bd
park1997 <iambyunghyun97@gmail.com>
2022-05-12 23:15:55 +0900

    Update CrawlingController.java

--- src/main/java/hello/hellospring/controller/CrawlingController.java
+++ src/main/java/hello/hellospring/controller/CrawlingController.java
@@ -47,8 +47,8 @@ public class CrawlingController {
         int interval =1000;
 
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-        options.addArguments("Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36");
+        options.addArguments("--headless");
+//        options.addArguments("Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36");
         options.addArguments("disable-gpu");
         options.addArguments("--disable-gpu");
         options.addArguments("lang=ko_KR");
